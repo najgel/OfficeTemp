65199389bb4f2e1fbcb901633147d979b83a95d8
Niklas Gustafsson <gustafsson.niklas@gmail.com>
2018-11-21 19:55:13 +0100

    Add project files.

---
+++ SerialConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SerialConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
